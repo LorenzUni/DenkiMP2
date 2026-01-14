7517f28fecb0020543219e03a4c88ef248c81f52
GoogleEngineer <lorenz.grasser@hotmail.com>
2026-01-14 16:28:02 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const declineButton = document.querySelector(".decline");
+  const acceptButton = document.querySelector(".accept");
+
 
   if (declineButton) {
     declineButton.addEventListener("click", function () {
       alert("Oh schade! Dann entgeht dir unser tolles personalisiertes Erlebnis 😢");
+    });
+  }
+  if (acceptButton) {
+    acceptButton.addEventListener("click", function () {
+      alert("Super! Du erhältst nun ein personalisiertes Erlebnis 🎉");
     });
   }
 });
